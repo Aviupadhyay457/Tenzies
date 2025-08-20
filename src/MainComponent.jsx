5a65fc17e9dd6c1a8d5feab7734d30617b0e535a
Aviupadhyay457 <avinashupadhyay457@gmail.com>
2025-08-20 23:32:51 +0530

    added a timer

--- src/MainComponent.jsx
+++ src/MainComponent.jsx
@@ -7,7 +7,24 @@ import Confetti from 'react-confetti'
 export default function MainComponent(){
     const[dice,setDice]=React.useState(()=>getInitialDice())
     const[count,setCount]=React.useState(0)
-
+    const[time , setTime]=React.useState(0)
+    React.useEffect(()=>{
+        let timer=setInterval(()=>setTime(oldtime=>oldtime+1),1000)
+        return function(){
+            timer.clearInterval(timer)
+        }
+    },[])
+    
+    function secondsToHms(d) {
+    d = Number(d);
+    var h = Math.floor(d / 3600);
+    var m = Math.floor(d % 3600 / 60);
+    var s = Math.floor(d % 3600 % 60);
+    var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+    var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+    var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+    return hDisplay + mDisplay + sDisplay; 
+    }
     const gameWon=dice.every(die=>die.isHeld===true && die.value===dice[0].value)
     function getInitialDice(){
         return new Array(10).fill(0).map(()=>
@@ -76,7 +93,7 @@ export default function MainComponent(){
             <button onClick={handleBtnClick}>{gameWon?"New Game":"Roll Dice"}</button>
             <section>
                 <p>Roll:{count}</p>
-                <p>Time:</p>
+                <p>Time:{secondsToHms(time)}</p>
             </section>
             {gameWon && <Confetti/>}
         </main>
